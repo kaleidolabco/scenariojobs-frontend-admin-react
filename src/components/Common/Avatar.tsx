eabd73567bb17dfb2fbe4c013b49a518f0356f15
juancuasquen08 <cuasquen08@gmail.com>
2026-07-27 20:27:15 -0500

    Se corrigen advertencias del componente de Avatar

--- src/components/Common/Avatar.tsx
+++ src/components/Common/Avatar.tsx
@@ -9,7 +9,7 @@ interface AvatarProps {
     placeholderClass?: string;
 }
 
-const avatarSVG = (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></g></svg>);
+const avatarSVG = (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></g></svg>);
 
 const Avatar: React.FC<AvatarProps> = ({
     src,
